82dc40e4920067631ee9fd7868b7ce04a01e11c9
a-small-coder <61427295+a-small-coder@users.noreply.github.com>
2023-06-27 13:01:47 +0300

    ✨ added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 
 import { Box } from '@mantine/core';
 import MainRoutes from './routes/MainRoutes';
+import { HeaderMenu } from './components/Header';
 
 function App() {
+
+  const links = [
+    {
+      link: '/posts',
+      label: 'Посты',
+      links: []
+    },
+    {
+      link: '/tasks',
+      label: 'Задачи',
+      links: []
+    },
+    {
+      link: '/photos',
+      label: 'Фото',
+      links: []
+    },
+  ]
+
   return (
     <Box>
+      <HeaderMenu links={links}/>
       <MainRoutes/>
     </Box>
   )
